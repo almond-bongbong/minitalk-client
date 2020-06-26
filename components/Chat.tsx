ce0dabbfeaae338e1940411b74983e4a08f3b79f
cmlee <bal.dongdongdong@gmail.com>
2020-06-27 00:34:47 +0900

    make chat component

---
+++ components/Chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, ScrollView, TextInput } from 'react-native';
+import gql from 'graphql-tag';
+
+function Chat() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+export default Chat;
